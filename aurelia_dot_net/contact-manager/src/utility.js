8612b952f8fb3070956bff6b20a1c41f4566f047
avar0 <avar0@hotmail.com>
2016-11-15 23:49:13 -0800

    setup structure

---
+++ aurelia_dot_net/contact-manager/src/utility.js
@@ -0,0 +1,9 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    function areEqual(obj1, obj2) {
+        return Object.keys(obj1).every(function (key) { return obj2.hasOwnProperty(key) && (obj1[key] === obj2[key]); });
+    }
+    exports.areEqual = areEqual;
+    ;
+});
+//# sourceMappingURL=utility.js.map
